956789293874cd705eb2f2fb4a3921c7240bdc30
Jesse Elder <elder099@cn1107.mangi.msi.umn.edu>
2021-06-17 19:10:14 -0500

    Quick directory change

--- SimandCall.sh
+++ SimandCall.sh
@@ -23,7 +23,7 @@ grep '^' Fastqlist.txt
 echo 'Made the list. Running DiscoSnp.'
 
 ##Run DiscoSNP++
-run_discoSnp++.sh -r /home/noyes046/shared/projects/SNP_Call_Benchmarking/DiscoSNP/Fastqlist.txt -T \
+run_discoSnp++.sh -r /home/noyes046/shared/projects/SNP_Call_Benchmarking/Fastqlist.txt -T \
 	-G /home/noyes046/shared/databases/megares_v2.0/megares_full_database_v2.00.fasta \
 	--bwa_path /panfs/roc/msisoft/bwa/0.7.17_gcc-7.2.0_haswell -p DiscoSNP_output/Mixed_Nova_short
 
