261271a67d491b66b81f61ab88fa95d2ee968896
elder099 <elder099@umn.edu>
2021-11-04 14:32:41 -0500

    New Directory structure

--- Workflow/JustGen.sh
+++ Workflow/JustGen.sh
@@ -8,38 +8,44 @@
 #####Script to create directory structure
 
 ###Shortcut variables
-Bench="/home/noyes046/shared/projects/SNP_Call_Benchmarking"
+Bench="/home/noyes046/shared/projects/SNP_Call_Benchmarking/Benchmarking_Run"
 #db="/home/noyes046/shared/databases/Jesse_database/Jesse_full_db.fasta"
 db="/home/noyes046/shared/databases/Jesse_database/Full_Sanchez_DB.fasta"
 
 
 
-mkdir "$Bench/Benchmarking_Run"
-mkdir "$Bench/Benchmarking_Run/SNP_Injector"
+mkdir "$Bench"
+mkdir "$Bench/SNP_Injector"
 
 
 datasets=("0.5" "1" "5" "10" "15" "25" "50") #name of datasets/directories
+subsets=("5" "15" "25" "35") #number of reference sequences subsampled
 
-for i in "${datasets[@]}"
+for j in "${subsets[@]}"
 do
-	echo "$i"
-	mkdir "$Bench/Benchmarking_Run/M$i"
-	mkdir "$Bench/Benchmarking_Run/M$i/Alignment"
-	mkdir "$Bench/Benchmarking_Run/M$i/Disco_Out"
-	mkdir "$Bench/Benchmarking_Run/M$i/Freq_Out"
-	mkdir "$Bench/Benchmarking_Run/M$i/GATK_Out"
-        mkdir "$Bench/Benchmarking_Run/M$i/FB_Out"
-        mkdir "$Bench/Benchmarking_Run/M$i/Samtools_Out"
-        mkdir "$Bench/Benchmarking_Run/M$i/VarDict_Out"
-        mkdir "$Bench/Benchmarking_Run/M$i/Ebwt_Out"
-        mkdir "$Bench/Benchmarking_Run/M$i/VarScan_Out"
-        mkdir "$Bench/Benchmarking_Run/M$i/MIDAS_Out"
+	echo "$j"
+	mkdir "$Bench/S$j" #Make subset directories
+
+	for i in "${datasets[@]}"
+	do
+		echo "$i"
+		mkdir "$Bench/S$j/M$i"  #Make dataset directories
+		mkdir "$Bench/S$j/M$i/Alignment"
+		mkdir "$Bench/S$j/M$i/Disco_Out"
+		mkdir "$Bench/S$j/M$i/Freq_Out"
+		mkdir "$Bench/S$j/M$i/GATK_Out"
+        	mkdir "$Bench/S$j/M$i/FB_Out"
+        	mkdir "$Bench/S$j/M$i/Samtools_Out"
+        	mkdir "$Bench/S$j/M$i/VarDict_Out"
+        	mkdir "$Bench/S$j/M$i/Ebwt_Out"
+        	mkdir "$Bench/S$j/M$i/VarScan_Out"
+        	mkdir "$Bench/S$j/M$i/MIDAS_Out"
 
 
-
-	printf "$Bench/Benchmarking_Run/M$i/Sim${i}_reads_R1.fastq\n$Bench/Benchmarking_Run/M$i/Sim${i}_reads_R2.fastq" > $Bench/Benchmarking_Run/M$i/Fastqlist_Disco.txt 
-	printf "$Bench/Benchmarking_Run/M$i/Alignment/Sim${i}_Ref_sorted.bam" > $Bench/Benchmarking_Run/M$i/meta_sample_list.txt
-	printf "Ecoli	$Bench/Benchmarking_Run/M$i/Sim${i}_reads_R1.fastq	$Bench/Benchmarking_Run/M$i/Sim${i}_reads_R2.fastq" > $Bench/Benchmarking_Run/M$i/Fastqlist_Bact.txt
+		printf "$Bench/S$j/M$i/Sim${i}_reads_R1.fastq\n$Bench/S$j/M$i/Sim${i}_reads_R2.fastq" > $Bench/S$j/M$i/Fastqlist_Disco.txt 
+		printf "$Bench/S$j/M$i/Alignment/Sim${i}_Ref_sorted.bam" > $Bench/S$j/M$i/meta_sample_list.txt
+		printf "Ecoli	$Bench/S$j/M$i/Sim${i}_reads_R1.fastq	$Bench/S$j/M$i/Sim${i}_reads_R2.fastq" > $Bench/S$j/M$i/Fastqlist_Bact.txt
+	done
 done
 
 
