eaa72fddcb51e372c90c689cc5c14a4922c3cff6
elder099 <elder099@umn.edu>
2021-11-04 10:41:02 -0500

    JustMetaSNV

--- Workflow/JustMetaSNV.sh
+++ Workflow/JustMetaSNV.sh
@@ -20,11 +20,11 @@ conda activate metasnv
 
 for i in "${datasets[@]}"
 do
-/usr/bin/time -v -o $Bench/Benchmarking_Run/M${i}/Meta_Out/Meta_Time.txt metaSNV.py $Bench/Benchmarking_Run/M${i}/Meta_Out \
+/usr/bin/time -v -o $Bench/Benchmarking_Run/M${i}/Meta_Time.txt metaSNV.py $Bench/Benchmarking_Run/M${i}/Meta_Out \
         $Bench/Benchmarking_Run/M${i}/meta_sample_list.txt \
         $db --threads 16
 
 
 cat $Bench/Benchmarking_Run/M${i}/Meta_Out/snpCaller/called_SNPs.best_split_* | sed -n -e 's/[0-9][0-9]*|//g; /[ACTG]/ s/|.*//p' > $Bench/Benchmarking_Run/M${i}/Meta_Out/Meta_Out_Fix.csv
-
+mv $Bench/Benchmarking_Run/M${i}/Meta_Time.txt $Bench/Benchmarking_Run/M${i}/Meta_Out/Meta_Time.txt
 done
