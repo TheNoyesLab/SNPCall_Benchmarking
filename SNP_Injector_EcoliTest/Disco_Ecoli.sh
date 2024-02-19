1ea61c108994e48c56c6f83f3259046f95fa9b69
elder099 <elder099@umn.edu>
2024-02-19 17:06:47 -0600

    Removing deprecated SNP_Injector directory

--- SNP_Injector_EcoliTest/Disco_Ecoli.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-run_discoSnp++.sh -r Fastqlist.txt \
-        -G  SNPReference.fasta\
-        --bwa_path /panfs/roc/msisoft/bwa/0.7.17_gcc-7.2.0_haswell -p Disco_Out
--- SNP_Injector_EcoliTest/SimSNP.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-iss generate --genomes SNPReference.fasta -n 1m --cpus 16 --mode perfect --output SimSNP_reads
